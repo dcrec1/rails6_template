2b521eac662db3b1530823e49866551a132c56bf
Diego Carrion <dc.rec1@gmail.com>
2010-06-15 00:36:16 -0300

    spork is a dependency

--- main.rb
+++ main.rb
@@ -15,6 +15,7 @@ gem 'cucumber', ">=0.6.3", :group => :cucumber
 gem 'cucumber-rails', ">=0.3.2", :group => :cucumber
 gem 'capybara', ">=0.3.6", :group => :cucumber
 gem 'database_cleaner', ">=0.5.0", :group => :cucumber
+gem 'spork', ">=0.8.4", :group => :cucumber
 
 gem 'inploy'
 
