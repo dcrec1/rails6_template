9427589d70c27d94924d282d27bf4ec6f0968a1a
Diego Carrion <dc.rec1@gmail.com>
2012-03-24 18:08:22 -0300

    updated urls

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 run "rm -Rf .gitignore README public/index.html test app/views/layouts/*"
 
-run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/rails31/Gemfile' -O Gemfile"
+run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/Gemfile' -O Gemfile"
 
 run "bundle install --binstubs"
 
@@ -21,15 +21,15 @@ generate "devise User"
 generate "devise Admin"
 generate "rails_admin:install"
 
-run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/rails31/gitignore' -O .gitignore"
-run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/rails31/application.html.haml' -O app/views/layouts/application.html.haml"
-run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/rails31/devise_steps.rb' -O features/step_definitions/devise_steps.rb"
-run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/rails31/users.rb' -O spec/factories/users.rb"
-run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/rails31/build.rake' -O lib/tasks/build.rake"
-run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/rails31/bootstrap.rake' -O lib/tasks/bootstrap.rake"
-run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/rails31/overlay.png' -O app/assets/images/overlay.png"
-run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/rails31/newrelic.yml' -O config/newrelic.yml"
-run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/rails31/grid.png' -O app/assets/images/grid.png"
+run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/gitignore' -O .gitignore"
+run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/application.html.haml' -O app/views/layouts/application.html.haml"
+run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/devise_steps.rb' -O features/step_definitions/devise_steps.rb"
+run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/users.rb' -O spec/factories/users.rb"
+run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/build.rake' -O lib/tasks/build.rake"
+run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/bootstrap.rake' -O lib/tasks/bootstrap.rake"
+run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/overlay.png' -O app/assets/images/overlay.png"
+run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/newrelic.yml' -O config/newrelic.yml"
+run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/grid.png' -O app/assets/images/grid.png"
 
 create_file 'config/deploy.rb', <<-DEPLOY
 application = '#{app_name}'
