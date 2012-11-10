90d0d7dc6b85f431664789fd34136bba89ebba16
Diego Carrion <dc.rec1@gmail.com>
2012-11-09 22:24:02 -0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -5,34 +5,31 @@ run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/
 run "bundle install --binstubs"
 
 application  <<-GENERATORS
-config.generators do |g|
-  g.template_engine :haml
-  g.test_framework  :rspec
-  g.fixture_replacement :factory_girl
-end
+    config.generators do |g|
+      g.template_engine :haml
+      g.test_framework  :rspec
+      g.fixture_replacement :factory_girl
+      g.helper false
+      g.assets false
+      g.view_specs false
+    end
 GENERATORS
 
 generate "rspec:install"
-generate "cucumber:install --capybara --rspec --spork"
-generate "pickle --path --email"
 generate "formtastic:install"
 generate "devise:install"
 generate "devise User"
 generate "devise Admin"
+generate "rails_admin:install"
 
 run "rake db:migrate"
 
-generate "rails_admin:install"
-
 run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/gitignore' -O .gitignore"
 run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/application.html.haml' -O app/views/layouts/application.html.haml"
-run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/devise_steps.rb' -O features/step_definitions/devise_steps.rb"
 run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/users.rb' -O spec/factories/users.rb"
 run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/build.rake' -O lib/tasks/build.rake"
 run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/bootstrap.rake' -O lib/tasks/bootstrap.rake"
-run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/overlay.png' -O app/assets/images/overlay.png"
 run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/newrelic.yml' -O config/newrelic.yml"
-run "wget --no-check-certificate 'https://github.com/dcrec1/rails3_template/raw/master/grid.png' -O app/assets/images/grid.png"
 
 create_file 'config/deploy.rb', <<-DEPLOY
 application = '#{app_name}'
